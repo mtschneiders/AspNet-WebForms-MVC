df83d4ff42c0c10d31ff4866dbdc642262566a8e
Mateus Artur Schneiders <mtschneiders@gmail.com>
2017-05-21 13:25:05 -0300

    Add project files.

---
+++ WebFormsAndMVC/Controllers/DummyController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Principal;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebFormsAndMVC.Controllers
+{
+    public class DummyController : Controller
+    {
+        // GET: Dummy
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            WindowsIdentity clientId = (WindowsIdentity)HttpContext.User.Identity;
+
+            return View();
+        }
+
+        [ChildActionOnly]
+        public ActionResult GetStuff()
+        {
+            return PartialView();
+        }
+    }
+}
